9cdf75e1af1e8760c716f85b8232e8a995b7a01e
Aaron.Nacua <alnacua@gmail.com>
2018-04-02 22:31:29 -0500

    all steps completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+
+  document.getElementById("root")
+);
 registerServiceWorker();
-
